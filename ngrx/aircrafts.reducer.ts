6579677cec3275a81eee1cd9bac36b0fe2d202ab
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-12 14:58:53 +0100

    Modify case success and error

--- ngrx/aircrafts.reducer.ts
+++ ngrx/aircrafts.reducer.ts
@@ -1,15 +1,15 @@
 import { Action } from "@ngrx/store";
 import { AircraftsState, AircraftsStateEnum, initState } from "./aircrafts.state";
-import { AircraftsActionTypes } from "./aircrafts.action";
+import { AircraftsActionTypes, AircraftsActions } from "./aircrafts.action";
 
 export function AircraftsReducer(state : AircraftsState = initState, action: Action){
     switch(action.type){
         case AircraftsActionTypes.GET_ALL_AIRCRAFTS :
             return {...state, dataState: AircraftsStateEnum.LOADING}
         case AircraftsActionTypes.GET_ALL_AIRCRAFTS_SUCCESS :
-            return {...state, dataState: AircraftsStateEnum.LOADED}
+            return {...state, dataState: AircraftsStateEnum.LOADED, aircrafts: (<AircraftsActions> action).payload}
         case AircraftsActionTypes.GET_ALL_AIRCRAFTS_ERROR :
-            return {...state, dataState: AircraftsStateEnum.ERROR}
+            return {...state, dataState: AircraftsStateEnum.ERROR, errorMessage: (<AircraftsActions> action).payload}
         default :
             return {...state}
     }
