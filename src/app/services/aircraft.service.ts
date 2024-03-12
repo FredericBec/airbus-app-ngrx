24f997414152c2bbbce4dabe831b383552e22315
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-12 11:47:49 +0100

    Add aircraft service

---
+++ src/app/services/aircraft.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Aircraft } from '../model/aircraft.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AircraftService {
+
+  constructor(private http : HttpClient) { }
+
+  public getAircrafts(): Observable<Aircraft[]>{
+    return this.http.get<Aircraft[]>(environment.host + "/aircrafts");
+  }
+
+  public getDesignedAircrafts(): Observable<Aircraft[]>{
+    return this.http.get<Aircraft[]>(environment.host + "/aircrafts?design=true");
+  }
+
+  public getDevelopmentAircrafts(): Observable<Aircraft[]>{
+    return this.http.get<Aircraft[]>(environment.host + "/aircrafts?developpment=true");
+  }
+
+  public getAircraftsByMsn(id: number): Observable<Aircraft>{
+    return this.http.get<Aircraft>(environment.host + "/aircrafts/" + id);
+  }
+}
