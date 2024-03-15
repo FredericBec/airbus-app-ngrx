07e74f61cbc9cb21d71f22db65edb85d5d5f7c13
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-15 16:06:16 +0100

    Add login reducer and effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { User } from './model/user.model';
+import { selectUser } from './ngrx/authenticate/login.selectors';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'airbus-app-ngrx';
+
+  //user$: Observable<User>;
+  constructor(private store: Store){
+    //this.user$ = this.store.pipe(select(selectUser));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,27 @@ import { AircraftsNavbarComponent } from './components/aircrafts/aircrafts-navba
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { AircraftsReducer } from 'ngrx/aircrafts.reducer';
-import { AircraftsEffects } from 'ngrx/aircrafts.effects';
+import { AircraftsReducer } from 'src/app/ngrx/aircrafts/aircrafts.reducer';
+import { AircraftsEffects } from 'src/app/ngrx/aircrafts/aircrafts.effects';
+import { LoginoutComponent } from './components/loginout/loginout.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginEffects } from './ngrx/authenticate/login.effects';
+import { LoginReducer } from './ngrx/authenticate/login.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
     AircraftsComponent,
-    AircraftsNavbarComponent
+    AircraftsNavbarComponent,
+    LoginoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({airbusState: AircraftsReducer}),
-    EffectsModule.forRoot([AircraftsEffects]),
+    ReactiveFormsModule,
+    StoreModule.forRoot({airbusState: AircraftsReducer, authState: LoginReducer}),
+    EffectsModule.forRoot([AircraftsEffects, LoginEffects]),
     StoreDevtoolsModule.instrument()
   ],
   providers: [],
