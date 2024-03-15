7c2721bae59dda40f4200eef0f44aa1059853e34
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-15 16:06:58 +0100

    Add user guard

---
+++ src/app/guards/user.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, map, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Store, select } from '@ngrx/store';
+import { isAuthenticate } from '../ngrx/authenticate/login.selectors';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+  constructor(private authService : AuthService, private router : Router, private store: Store){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+   
+      return this.store.pipe(
+        select(isAuthenticate),
+        map(isAuthenticate => {
+          if(isAuthenticate === undefined || isAuthenticate === null){
+            return true;
+          }else if(isAuthenticate){
+            return true;
+          }else {
+            this.router.navigateByUrl('/login');
+            return false;
+          }
+        })
+      );
+  }
+  
+}
