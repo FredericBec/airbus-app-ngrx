2f2eca6a59ad2d8b51a0a703b32a3e0a1e07568a
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-15 16:05:32 +0100

    Add login path and user guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AircraftsComponent } from './components/aircrafts/aircrafts.component';
+import { UserGuard } from './guards/user.guard';
+import { LoginoutComponent } from './components/loginout/loginout.component';
 
 const routes: Routes = [
-  {path: "aircrafts", component: AircraftsComponent}
+  {path: "aircrafts", component: AircraftsComponent, canActivate: [UserGuard]},
+  {path: "login", component : LoginoutComponent},
+  {path: "", redirectTo: "aircrafts", pathMatch: "full"}
 ];
 
 @NgModule({
