6b35e273c8cff1624d505bda98912ae36f8cadae
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-18 12:59:02 +0100

    Add logout function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { User } from './model/user.model';
-import { selectUser } from './ngrx/authenticate/login.selectors';
+import { selectLogin, selectUser } from './ngrx/authenticate/login.selectors';
+import { LoginState } from './ngrx/authenticate/login.state';
+import { OnLogOut } from './ngrx/authenticate/login.action';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'airbus-app-ngrx';
 
-  //user$: Observable<User>;
+  authState$ : Observable<LoginState> | null = null;
+  isAuthenticate: boolean = false;
+  user: User | null = null;
   constructor(private store: Store){
-    //this.user$ = this.store.pipe(select(selectUser));
+    this.authState$ = this.store.pipe(select(selectLogin));
+  }
+
+  ngOnInit(){
+    this.authState$?.subscribe(
+      (state) => {
+        this.isAuthenticate = state.isAuthenticate;
+        this.user = state.user;
+      }
+    )
+  }
+
+  logOut(){
+    this.store.dispatch(new OnLogOut({}));
   }
 }
