8ecee1285711c20b554f0e3228e5f33a745f0b09
Frederic Bec <bec.frederic90@outlook.fr>
2024-03-22 08:13:04 +0100

    Remove unused function

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,24 +17,6 @@ export class AuthService {
     return this.http.get<User[]>(environment.host + "/users?email=" + email + "&password=" + password);
   }
 
-  getUser(){
-    let user = localStorage.getItem('user');
-    if(user){
-      this.userConnected = JSON.parse(atob(user));
-    }
-    return this.userConnected;
-  }
-
-  idUser(){
-    let user = this.getUser();
-    if(user){
-      if(user?.roles.length > 0){
-        if(user.roles.indexOf('User') > -1) return true; 
-      }
-    }
-    return false;
-  }
-
   setUser(user: User): any{
     localStorage.setItem('user', btoa(JSON.stringify(user)));
   }
